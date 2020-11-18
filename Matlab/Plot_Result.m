abe95afcea0be2a4ea3dbab38c6aa09bf2b3f675
Sepehr Ramezani <ramezani.dan@gmail.com>
2020-11-18 16:09:42 -0500

    Update

--- Matlab/Plot_Result.m
+++ Matlab/Plot_Result.m
@@ -11,10 +11,12 @@ M_ThresholdMin=10*3.14/180;
 M_ThresholdMax=90*3.14/180;
 ForceRatio=0.4;
 TrialCounter=0;
-SimMusclename=["bflh_r","bfsh_r","gaslat_r","gasmed_r","grac_r","recfem_r","sart_r","semimem_r","semiten_r","vasint_r","vaslat_r","vasmed_r"];
+readflage=0;
+
+SimMusclename=["bflh_r","semiten_r","gasmed_r","recfem_r","vaslat_r","vasmed_r","gaslat_r","gasmed_r","grac_r","sart_r","semimem_r","soleus_r","tfl_r","vasint_r","bfsh_r"];
 ExpMuscle=["RBICF","RSEMT","RMGAS","RRECF","RVASL","RVASM"];
         %%
-        
+if readflage        
         for T1=1:length(Terials1)
             for T2=1:length(Terials2)
                 TrialCounter=TrialCounter+1;
@@ -95,73 +97,82 @@ ExpMuscle=["RBICF","RSEMT","RMGAS","RRECF","RVASL","RVASM"];
           ResultData.('MaxEMG').colheaders= ExpMuscle;
           ResultData.('MaxEMG').data=max(MaxEMG);
           save ([folder '\Data\FinalData_Seperated.mat'],'ResultData');
-          
+end          
+load ([folder '\Data\FinalData_Seperated.mat']);
             %%
 for A=2:length(AnalyzeMethod)
     for m=1:length(Modelname)
         results_folder = append(folder,"Result\",Modelname(m),"\");
         %   IK_file=append(results_folder,name,'_ik.mot');
         %   NewExForcesetup=append(results_folder,'New_subject01_walk1_grf.xml');
-        for T1=2:length(Terials1)
-            for T2=5:length(Terials2)
+        for T1=1:length(Terials1)
+            for T2=6:length(Terials2)
                 filename=append(Terials1(T1),"_",Terials2(T2));
-                results_folder2=append(results_folder,AnalyzeMethod(A),"\",filename,"\");
-  
-
-           
-      
-                %% read CMC files
-                ActuateForce=importdata(append(results_folder2,Modelname(m),"_",filename,"_",AnalyzeMethod(A),"_Actuation_force.sto"));
-                MuscleActivation=importdata(append(results_folder2,Modelname(m),"_",filename,"_",AnalyzeMethod(A),"_controls.sto"));
-                Simtime=ActuateForce.data(:,1); %Simulation time
-                ExpRtime=FTable.data(:,1);      %Experimental time
-%                 RefT=0:.05:100;
-% %                 ResultData=cell(5,11,3);
-%                 %MuscleForce=
-%                 
-                for k=1:length(Stime)
+                for itr=1:length(fieldnames(ResultData.(filename).time))
+                    results_folder2=append(results_folder,AnalyzeMethod(A),"\",filename,"\",Terials3(itr),"\");
+                    ActuateForce=importdata(append(results_folder2,Modelname(m),'_',filename,'_',Terials3(itr),'_CMC','_Actuation_force.sto'));
+                    MuscleActivation=importdata(append(results_folder2,Modelname(m),'_',filename,'_',Terials3(itr),'_CMC','_controls.sto'));
+                    Simtime=ActuateForce.data(1:(end-1),1); %Simulation time
+%                     ExpRtime=FTable.data(:,1);      %Experimental time
+                    ExpRtime=ResultData.(filename).('time').(Terials3(itr));
+                    %                 
+                    % %                 ResultData=cell(5,11,3);
+                    %                 %MuscleForce=
+                    %
+%                     y=interp1(data.data(ww+1:kk,ii-1),data.data(ww+1:kk,ii),[ts:DStime:te],'linear','extrap');
 %                     
-                    Simindx=find(Simtime>=Stime(k)&Simtime<=Etime(k));
-                    Expindx=find(ExpRtime>=Stime(k)&ExpRtime<=Etime(k));
-                  figure  
-                    plot(Simtime(Simindx),MuscleActivation.data(Simindx,2),ExpRtime(Expindx),normalize(EMGtable.data(Expindx,2),'range'))
+%                     Simindx=find(Simtime>=Stime(k)&Simtime<=Etime(k));
+%                     Expindx=find(ExpRtime>=Stime(k)&ExpRtime<=Etime(k));
+%                   figure  
+%                     plot(Simtime(Simindx),MuscleActivation.data(Simindx,2),ExpRtime(Expindx),normalize(EMGtable.data(Expindx,2),'range'))
 %                     
 %                     RefT=ExpRtime(Expindx);
-%                     SimNormalT=((Simtime(Simindx)-Simtime(Simindx(1)))./(Simtime(Simindx(end))-Simtime(Simindx(1)))).*100;
-%                     ExpNormalT=((ExpRtime(Expindx)-ExpRtime(Expindx(1)))./(ExpRtime(Expindx(end))-ExpRtime(Expindx(1)))).*100;
-%                     ResultData.(filename).('time')(:,k)=RefT;
-%                     for Mu=1:length(SimMusclename)
-%                         ResultData.(filename).('Muscle_Force').(SimMusclename(Mu))(:,k)=interp1(ANormalT,ActuateForce.data(Simindx,Mu+1),RefT');
-%                         ResultData.(filename).('Muscle_Activation').(SimMusclename(Mu))(:,k)=interp1(Simtime(Simindx),MuscleActivation.data(Simindx,Mu+1),RefT');
-% %                         if 
-%                        
-%                     end
-
-
-%                     
+                     RefT=0:.1:100;
+                     SimNormalT=((Simtime-Simtime(1))./(Simtime(end)-Simtime(1))).*100;
+                     
+                     y=interp1(SimNormalT,MuscleActivation.data(1:(end-1),:),RefT','linear','extrap');
+                     y1=interp1(SimNormalT,ActuateForce.data(1:(end-1),:),RefT','linear','extrap');
+                     for Flmus=1:length(SimMusclename)
+                         ResultData.(filename).('NormalSimEMG').(SimMusclename(Flmus))(:,itr)=y(:,strncmp(MuscleActivation.colheaders,SimMusclename(Flmus),5));
+                         ResultData.(filename).('NormalSimForce').(SimMusclename(Flmus))(:,itr)=y1(:,strncmp(ActuateForce.colheaders,SimMusclename(Flmus),5));
+                     end
+                     ExpNormalT=((ExpRtime-ExpRtime(1))./(ExpRtime(end)-ExpRtime(1))).*100;
+                   
+                     for Flexmus=1:length(ExpMuscle)
+                        y3=interp1(ExpNormalT,ResultData.(filename).ExpEMG.(ExpMuscle(Flexmus)).(Terials3(itr)),RefT','linear','extrap');
+                        ResultData.(filename).('NormalExpEMG').(ExpMuscle(Flexmus))(:,itr)=y3;
+                     end
+                      
                 end
-%                 figure
-%                 x=0:0.02:4;
-%                 RA1=mean(ResultData{2,:}(:,2))-std(ResultData(:,1:4),0,2);
-%                 RA2=mean(ResultData(:,1:4),2)+std(ResultData(:,1:4),0,2);
-%                 RA3=mean(ResultData(:,1:4),2);
-%                 LA1=mean(LForceData(:,1:4),2)-std(LForceData(:,1:4),0,2);
-%                 LA2=mean(LForceData(:,1:4),2)+std(LForceData(:,1:4),0,2);
-%                 LA3=mean(LForceData(:,1:4),2);
-%                 x2 = [x, fliplr(x)];
-%                 RABetween = [RA1', fliplr(RA2')];
-%                 fill(x2, RABetween,[0.6 1 0.6]*.8,'EdgeAlpha',0,'FaceAlpha',0.2)
-%                 LABetween = [LA1', fliplr(LA2')];
-%                 hold on
-%                 fill(x2, LABetween,[1 0.5 1]*.8,'EdgeAlpha',0,'FaceAlpha',0.2)
-%                 title('Mean +/- sd  (Vastus lateralis Force Without Barce)');
-%                 
-%                 xlabel('Time (s)');
-%                 ylabel('Force (N)');
-%                 plot(x,RA3,'color',[[0.4660 0.6740 0.1880]]);
-%                 plot(x,LA3,'color',[0.4940 0.1840 0.5560]);
-%                 legend('SD of Left','SD of Right','Mean of Left','Mean of Right');
-%                 hold off
+                for Flexmus=1:length(ExpMuscle)
+                figure
+                x=RefT;
+                ExpMucs=ResultData.(filename).NormalExpEMG.(ExpMuscle(Flexmus))/ResultData.MaxEMG.data(Flexmus);
+                SimMusc=ResultData.(filename).NormalSimEMG.(SimMusclename(Flexmus));
+%                 if strncmp(Terials1(T1),"Fl",2)
+                RA1=mean(ExpMucs,2)-std(ExpMucs,0,2);
+                RA2=mean(ExpMucs,2)+std(ExpMucs,0,2);
+                RA3=mean(ExpMucs,2);
+                LA1=mean(SimMusc,2)-std(SimMusc,0,2);
+                LA2=mean(SimMusc,2)+std(SimMusc,0,2);
+                LA3=mean(SimMusc,2);
+                
+                x2 = [x, fliplr(x)];
+                RABetween = [RA1', fliplr(RA2')];
+                fill(x2, RABetween,[0.6 1 0.6]*.8,'EdgeAlpha',0,'FaceAlpha',0.2)
+                LABetween = [LA1', fliplr(LA2')];
+                hold on
+                fill(x2, LABetween,[1 0.5 1]*.8,'EdgeAlpha',0,'FaceAlpha',0.2)
+                title([filename SimMusclename(Flexmus)]);
+                
+                xlabel('Normalized Time (%)');
+                ylabel('activation ');
+                plot(x,RA3,'color',[[0.4660 0.6740 0.1880]]);
+                plot(x,LA3,'color',[0.4940 0.1840 0.5560]);
+                legend('SD of Exp','SD of Sim','Mean of Exp','Mean of Sim');
+                hold off
+                end
+%                 end
             end
         end
     end
