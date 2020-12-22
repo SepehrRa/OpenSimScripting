365d9f085d8c5d92c1fe0163a85bc9985164e8fa
Sepehr Ramezani <ramezani.dan@gmail.com>
2020-12-22 18:18:21 -0500

    Makeing inputs independent from header

--- Matlab/EventDetection.m
+++ Matlab/EventDetection.m
@@ -1,11 +1,11 @@
-function Events = EventDetection(Trialname,FTable,ForceRatio,MTable,Threshold)
+function Events = EventDetection(Trialname,Time,FTable,ForceRatio,MTable,Threshold)
 if contains(Trialname,"IsoM")
-    [indx,c]=find(abs(FTable(:,10))>ForceRatio.*max(abs(FTable(:,10))));
-    Stime=FTable(indx([1;find(diff(indx)>10)+1]),1);
-    Etime=FTable(indx([(find(diff(indx)>10));end]),1);
+    [indx,c]=find(abs(FTable)>ForceRatio.*max(abs(FTable)));
+    Stime=Time(indx([1;find(diff(indx)>10)+1]),1);
+    Etime=Time(indx([(find(diff(indx)>10));end]),1);
 elseif contains(Trialname,"IsoK") 
     if contains(Trialname,"Fl")
-        [indx,c]=find(MTable(:,6)>Threshold(1) & MTable(:,6)<Threshold(2));
+        [indx,c]=find(MTable>Threshold(1) & MTable<Threshold(2));
         % & [0;diff(MTable(:,6))>0]
         Sindx=indx([1;find(diff(indx)>10)+1]);
         Eindx=indx([(find(diff(indx)>10));end]);
@@ -13,24 +13,24 @@ elseif contains(Trialname,"IsoK")
         Stime=[];
         Etime=[];
         for ww=1:length(Sindx)
-            if (MTable(Sindx(ww),6)-MTable(Sindx(ww)-1,6))>0
+            if (MTable(Sindx(ww))-MTable(Sindx(ww)-1))>0
                 count=count+1;
-                Stime=[Stime;FTable(Sindx(ww),1)];
-                Etime=[Etime;FTable(Eindx(ww),1)];
+                Stime=[Stime;Time(Sindx(ww),1)];
+                Etime=[Etime;Time(Eindx(ww),1)];
             end
         end
     else
-        [indx,c]=find(MTable(:,6)>Threshold(1) & MTable(:,6)<Threshold(2));
+        [indx,c]=find(MTable>Threshold(1) & MTable<Threshold(2));
         Sindx=indx([1;find(diff(indx)>10)+1]);
         Eindx=indx([(find(diff(indx)>10));end]);
         count=0;
         Stime=[];
         Etime=[];
         for ww=1:length(Sindx)
-            if (MTable(Sindx(ww)+1,6)-MTable(Sindx(ww),6))<0
+            if (MTable(Sindx(ww)+1)-MTable(Sindx(ww)))<0
                 count=count+1;
-                Stime=[Stime;FTable(Sindx(ww),1)];
-                Etime=[Etime;FTable(Eindx(ww),1)];
+                Stime=[Stime;Time(Sindx(ww),1)];
+                Etime=[Etime;Time(Eindx(ww),1)];
             end
         end
     end
