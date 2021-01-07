e054b16c837308667e218e1c66b84b7413e414c5
Sepehr Ramezani <ramezani.dan@gmail.com>
2021-01-07 04:51:06 -0500

    Solution for IsoK 120 180 240 problem
    in the ex IsoM 120 180 240 range of motion was low but after increasing thereshold gait event algorithm had problem with angle around 90 bcause in experiment participant reaches les than 75 deg. to solve this I added 5/180*3.14 in ex_IsoM trails.

--- Matlab/EventDetection.m
+++ Matlab/EventDetection.m
@@ -2,10 +2,11 @@ function Events = EventDetection(Trialname,Time,FTable,ForceRatio,MTable,Thresho
 if contains(Trialname,"IsoM")
     [indx,c]=find(abs(FTable)>ForceRatio.*max(abs(FTable)));
     Stime=Time(indx([1;find(diff(indx)>10)+1]),1);
-    Etime=Time(indx([(find(diff(indx)>10));end]),1);
+    %Etime=Time(indx([(find(diff(indx)>10));end]),1);
+    Etime=Stime+3;
 elseif contains(Trialname,"IsoK") 
     if contains(Trialname,"Fl")
-        [indx,c]=find(MTable>Threshold(1) & MTable<Threshold(2));
+        [indx,c]=find(MTable>=Threshold(1) & MTable<=Threshold(2));
         % & [0;diff(MTable(:,6))>0]
         Sindx=indx([1;find(diff(indx)>10)+1]);
         Eindx=indx([(find(diff(indx)>10));end]);
@@ -20,7 +21,7 @@ elseif contains(Trialname,"IsoK")
             end
         end
     else
-        [indx,c]=find(MTable>Threshold(1) & MTable<Threshold(2));
+        [indx,c]=find(MTable>=Threshold(1) & MTable<=(Threshold(2)-5/180*3.14));
         Sindx=indx([1;find(diff(indx)>10)+1]);
         Eindx=indx([(find(diff(indx)>10));end]);
         count=0;
